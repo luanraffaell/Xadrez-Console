e11feda5398f3df9fe386478678473c9003baf22
Luan Rafael <luanraffaell@hotmail.com>
2021-08-24 19:50:29 -0300

    Comecando a implementar o tabuleio

--- Xadrez-Console/Program.cs
+++ Xadrez-Console/Program.cs
@@ -7,8 +7,7 @@ namespace Xadrez_Console
     {
         static void Main(string[] args)
         {
-            Posicao p = new Posicao(3, 4);
-            Console.WriteLine(p);
+            Tabuleiro tab = new Tabuleiro(8, 8);
             
         }
     }
---
+++ Xadrez-Console/tabuleiro/Peca.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor cor { get; protected set; }
+        public int qteMovimentos { get; protected set; }
+        public Tabuleiro tab { get; protected set; }
+
+        //tabuleiro pode ter posicao, cor e quantidade de movimento
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tab)
+        {
+            this.posicao = posicao;
+            this.cor = cor;
+            this.tab = tab;
+            this.qteMovimentos = 0; // toda peça inicia com zero movimento
+        }
+    }
+}
