d62375e2c25f0e1d93575ec29218a7ea8d530a43
Luan Rafael <luanraffaell@hotmail.com>
2021-08-24 19:40:03 -0300

    Criando classe posicao

---
+++ Xadrez-Console/tabuleiro/Posicao.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/*cada peça do tabuleiro se encontra em uma posição e tenho que representar isso.*/
+
+namespace tabuleiro
+{
+    class Posicao
+    {
+        public int Linha { get; set; } //uma posição é dizer em qual linha e coluna a peça está
+        public int Coluna { get; set; }
+
+        public Posicao(int linha, int coluna)
+        {
+            Linha = linha;
+            Coluna = coluna;
+        }
+        public override string ToString()
+        {
+            return Linha + ", "+Coluna;
+        }
+    }
+}
